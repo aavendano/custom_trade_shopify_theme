3a0a20e8a56f2d6420acfde606f83c82c983d122
Alejandro Avendano <aavendano79@gmail.com>
2025-09-09 00:24:26 -0400

    feat: update Tailwind CSS styles and integrate Preline UI
    - Added new utility classes to tailwind-output.css for layout, spacing, typography, and shadows.
    - Updated package-lock.json and package.json to include Preline UI and Tailwind CSS forms plugin.
    - Modified input.css to import Preline UI and its variants.
    - Enhanced tailwind.config.js to include Preline UI scripts and extend theme colors.
    - Updated 404.json template to include new custom sections and collections for better content organization.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     './layout/*.liquid',
-    './sections/*.liquid', 
+    './sections/*.liquid',
     './snippets/*.liquid',
     './templates/*.liquid',
-    './blocks/*.liquid'
+    './blocks/*.liquid',
+    'node_modules/preline/dist/*.js'
   ],
   prefix: 'tw-',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'plt-purple': '#533278',
+        'plt-pink': '#F4436C',
+        'plt-gray': '#B4B4B4',
+        'plt-teal': '#0C7489',
+        'plt-gray-green': '#545C52',
+        'plt-black': '#000000',
+        'plt-white': '#FFFFFF',
+        'plt-light-gray': '#F4F4F4',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('./node_modules/preline/dist/index.js'),
+  ],
 }
