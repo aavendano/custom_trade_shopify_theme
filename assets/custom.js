124bfbc85fa2525863349574b9f9d4c8de2f1bb8
Alejandro Avendano <aavendano79@gmail.com>
2025-10-09 01:32:49 -0400

    merge live branch

--- assets/custom.js
+++ assets/custom.js
@@ -1,2 +1,10 @@
-// Custom JavaScript file for theme functionality
-// TwicPics web components have been removed - using native implementation instead
+import { installTwicpics, TwicImg, TwicPicture, TwicVideo } from '@twicpics/components/webcomponents';
+import '@twicpics/components/style.css';
+
+installTwicpics({
+  "domain": "https://playlovetoys.twic.pics"
+});
+
+customElements.define('twic-img', TwicImg);
+customElements.define('twic-picture', TwicPicture);
+customElements.define('twic-video', TwicVideo);
