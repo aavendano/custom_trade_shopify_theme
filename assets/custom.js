32b09e26a98ef6d7e48ad90ea1e32f6d21e4e5cd
Alejandro Avendano <aavendano79@gmail.com>
2025-10-24 01:12:58 -0400

    live pull

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,13 @@
-// Custom JavaScript file for theme functionality
-// TwicPics web components have been removed - using native implementation instead
+import { installTwicpics, TwicImg, TwicPicture, TwicVideo } from '@twicpics/components/webcomponents';
+import '@twicpics/components/style.css';
 
+installTwicpics({
+    "domain": "https://playlovetoys.twic.pics"
+});
+
+customElements.define('twic-img', TwicImg);
+customElements.define('twic-picture', TwicPicture);
+customElements.define('twic-video', TwicVideo);
 // Safe localStorage wrapper for sandboxed environments
 function safeLocalStorage() {
     try {
